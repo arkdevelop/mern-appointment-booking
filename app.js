d8efe0f68959c2e52aa08ef7ca5d073a196e3cf9
arkdevelop <arkdevelop@users.noreply.github.com>
2021-04-11 17:29:48 -0400

    user info

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const bodyParser = require("body-parser");
 const graphQLHTTP = require("express-graphql");
 const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const dburi = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@appointments.hxov7.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 const Event = require("./models/event");
+const User = require("./models/user");
 
 const app = express();
 
@@ -24,11 +26,22 @@ app.use('/api', graphQLHTTP.graphqlHTTP({
             date: String!
         }
 
+        type User {
+            _id: ID!
+            email: String!
+            password: String
+        }
+
         input EventInput {
             title: String!
             description: String!
             price: Float!
             date: String!
+        }
+
+        input UserInput {
+            email: String!
+            password: String!
         }
 
         type RootQuery {
@@ -37,6 +50,7 @@ app.use('/api', graphQLHTTP.graphqlHTTP({
 
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
     
         schema {
@@ -70,6 +84,21 @@ app.use('/api', graphQLHTTP.graphqlHTTP({
                 console.log(err);
                 throw err;
             });
+        },
+        createUser: (args) => {
+            // Must return so GraphQL knows to wait for async to finish
+            return bcrypt.hash(args.userInput.password, 12).then(hashedPassword => {
+                const user = new User({
+                    email: args.userInput.email,
+                    password: args.hashedPassword
+                });
+                return user.save();
+            }).then(result => {
+                return {... result._doc, _id: result.id}
+            }).catch(err => {
+                throw err;
+            });
+            
         }
     },
     graphiql: true //Debugging
